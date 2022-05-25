84222a1076255ef5157e701b846a5cc6d33e0901
PedroPiuma <lpap1000@gmail.com>
2022-05-25 15:15:02 -0300

    add menu mobile

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import { AuthProvider, RequireAuth } from "./context/auth-context";
 import Login from "./routes/Login";
 import Layout from "./components/Layout";
-import ProtectedPage from "./routes/ProtectedPage";
+import Home from "./routes/Home";
 import SignUp from "./routes/SignUp";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        {/* <Route element={<Layout />}> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={
-          <RequireAuth>
-            <ProtectedPage />
-          </RequireAuth>
-        }
-        />
-        {/* </Route> */}
+        <Route element={<Layout />}>
+          <Route path="/" element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+          />
+        </Route>
       </Routes>
     </AuthProvider>
   );
